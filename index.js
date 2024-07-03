a2d392b14f093706cab2747edaacd4f32bf0ec4b
Alex Handley <smithvillescholarsah@gmail.com>
2024-07-02 19:43:44 -0500

    S13M3-Project completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require("./api/server.js");
+
+const port = 5000;
+
+server.listen(port, function() {
+    console.log(`*** Server listening on: http://localhost:${port} ***`);
+});
